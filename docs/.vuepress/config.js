ebb8cd4c7e705e370e92014d2926f4edf4e4a44e
lzl <lzl102872@163.com>
2020-04-15 11:28:17 +0800

    edit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -177,7 +177,7 @@ module.exports = {
                 children: [
                     '/example/css/descartes', // 笛卡尔心型曲线
                     '/example/page/layout', // 页面布局
-                    '/example/css/amplification', // 悬停放大图片效果
+                    '/example/css/amplification', // 图片效果
                 ]
             }]
         }
